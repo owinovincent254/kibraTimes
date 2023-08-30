0a9219034e1e6f189ab8da61164eb25e697624f4
vincent owino <owinovincent589@gmail.com>
2023-08-30 10:52:19 +0300

    installed cors and used it

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const postRoutes=require("./routes/post")
 const authRoutes=require("./routes/auth")
 const express = require("express");
 const mongoose = require("mongoose");
-const dotenv=require("dotenv")
+const dotenv=require("dotenv");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 6002;
+app.use(cors())
 app.use(express.json())
 app.use("/api/posts",postRoutes)
 app.use("/api/auth",authRoutes)
